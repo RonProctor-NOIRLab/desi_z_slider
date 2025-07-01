cc4ff5c5c92dac90c1a8bf4d7bb58cc01390166e
RonProctor-NOIRLab <ronald.proctor@noirlab.edu>
2025-07-01 13:50:19 -0700

    Update player.js

--- player.js
+++ player.js
@@ -24,7 +24,7 @@ playPause.addEventListener('click', () => {
     playInterval = setInterval(() => {
       slider.value = (+slider.value + 1) % totalSlices;
       viewer.src = images[+slider.value].src;
-    }, 100); // speed: 100 ms per frame
+    }, 10); // speed: 10 ms per frame
   } else {
     playing = false;
     playPause.textContent = '▶️ Play';
